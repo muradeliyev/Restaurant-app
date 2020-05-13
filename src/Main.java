4b2291f0be3a68ccdd7f57cf2e982545d54e0e1c
Murad Eliyev <muradeliyev27032002@gmail.com>
2020-05-13 23:37:31 +0400

    adding the skeleton

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,42 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Main extends JFrame implements MouseListener {
     public static void main(String args[]) {
+
+    }
+
+    Main() {
+        super("Restaurant");
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
