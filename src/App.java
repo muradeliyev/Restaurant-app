f21a753402c22c78caebdce2482e1ef788f214ce
Murad Eliyev <muradeliyev27032002@gmail.com>
2020-05-17 00:08:17 +0400

    welcome page UI is ready

--- src/App.java
+++ src/App.java
@@ -1,38 +1,43 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-public class App extends JFrame implements MouseListener {
+public class App extends JFrame {
+    GridBagConstraints gbc;
+
     App() {
         super("Restaurant");
         setLayout(new FlowLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 500);
+        create();
+        setVisible(true);
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
+    public static void main(String[] args) {
+        App app = new App();
     }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
+    void create() {
+        gbc = new GridBagConstraints();
 
-    }
+        JPanel main = new JPanel();
+        main.setLayout(new GridBagLayout());
+//        main.setBackground(Color.CYAN);
+        main.setOpaque(true);
 
-    @Override
-    public void mouseReleased(MouseEvent e) {
+        gbc.gridy = 0; main.add(new JLabel("<html><h1 style='padding:10px;'>Welcome</h1></html>"), gbc);
+        gbc.weightx = 1.0; gbc.anchor = GridBagConstraints.WEST;
+        gbc.gridy = 1; main.add(new JLabel("<html><span style='text-align:left;width:100%;'>email</span></html>"), gbc);
+        gbc.gridy = 2; main.add(new JTextField(10), gbc);
 
-    }
+        gbc.gridy = 3; main.add(new JLabel("password"), gbc);
+        gbc.gridy = 4; main.add(new JPasswordField(10), gbc);
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
+        gbc.gridy = 5; main.add(new JButton("login"), gbc);
+        gbc.gridy = 6; main.add(new JLabel("<html><i>or</i></html>"), gbc);
+        gbc.gridy = 7; main.add(new JButton("create an account"), gbc);
 
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
+        add(main);
     }
 }
--- src/Restaurant.java
+++ src/Restaurant.java
@@ -21,70 +21,71 @@ class Restaurant extends JFrame {
 
     void Welcome() {
         // this is the initial welcome page with log in option
-        JPanel welcomePanel = new JPanel();
-        // applying vertical alignment for items
-        welcomePanel.setLayout(new GridLayout(8, 1));
+        GridBagConstraints gbc = new GridBagConstraints();
 
+        JPanel welcomePanel = new JPanel();
+        welcomePanel.setLayout(new GridBagLayout());
         welcomePanel.setOpaque(true);
         welcomePanel.setBorder(new EmptyBorder(30, 10, 10, 10));
-//        welcomePanel.setBackground(Color.black);
+//        welcomePanel.setBackground(Color.white);
 
         // Login header
         JLabel label = new JLabel("Log in");
         label.setHorizontalAlignment(SwingConstants.CENTER);
-//        label.setBorder(new EmptyBorder(0, 0, 25, 0));
         label.setFont(new Font("Arial", Font.PLAIN, 30));
-        label.setOpaque(true);
-//        label.setBackground(Color.RED);
+        gbc.gridy = 0; welcomePanel.add(label, gbc);
 
-        // container panel for inputs for login /////////////////////
-//        JPanel inputPanel = new JPanel();
-//        inputPanel.setOpaque(true);
-//        inputPanel.setBackground(Color.CYAN);
+        gbc.weightx = 1.0; gbc.anchor = GridBagConstraints.WEST;
 
         // email label and textfield for logging in
         JLabel emaillabel = new JLabel("email");
-        emaillabel.setBorder(new EmptyBorder(20, 0, 5, 0));
+        emaillabel.setFont(new Font("Arial", Font.BOLD, 12));
+        emaillabel.setBorder(new EmptyBorder(30, 0, 5, 0));
+        emaillabel.setForeground(Color.darkGray);
+        gbc.gridy = 1; welcomePanel.add(emaillabel, gbc);
         JTextField email = new JTextField(20);
+        email.setBorder(new EmptyBorder(10, 10, 10, 10));
+        email.setOpaque(true); email.setBackground(Color.lightGray);
+        gbc.gridy = 2; welcomePanel.add(email, gbc);
+
 
         // password label and textfield for logging in
         JLabel psswlabel = new JLabel("password");
+        psswlabel.setFont(new Font("Arial", Font.BOLD, 12));
+        psswlabel.setForeground(Color.darkGray);
         psswlabel.setBorder(new EmptyBorder(20, 0, 5, 0));
+        gbc.gridy = 3; welcomePanel.add(psswlabel, gbc);
         JPasswordField password = new JPasswordField(20);
+        password.setBorder(new EmptyBorder(10, 10, 10, 10));
+        password.setOpaque(true); password.setBackground(Color.lightGray);
+        gbc.gridy = 4; welcomePanel.add(password, gbc);
 
-//        inputPanel.add(emaillabel);
-//        inputPanel.add(email);
 
-//        inputPanel.add(psswlabel);
-//        inputPanel.add(password);
-
-        //////////////////////////////////////////////////////////////////
         // login button
         JButton login = new JButton("Log in");
         login.setHorizontalAlignment(SwingConstants.CENTER);
-//        login.setBorder(new EmptyBorder(20, 10, 10, 0));
-        login.setOpaque(true);
+        login.setOpaque(true); login.setBackground(Color.darkGray); login.setForeground(Color.white);
+        login.setFocusPainted(false); login.setRolloverEnabled(false);
+        login.setBorder(new EmptyBorder(10, 0, 10, 0));
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(20, 0, 0, 0);
+        gbc.gridy = 5; welcomePanel.add(login, gbc);
 
         // or sign in
-        JLabel or = new JLabel("or");
-        or.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel or = new JLabel("<html><h2>or</h2></html>");
+        gbc.anchor = GridBagConstraints.CENTER; gbc.insets = new Insets(0, 0, 0, 0);
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.gridy = 6; welcomePanel.add(or, gbc);
 
         // basically signing in and creating new account are the same things
         JButton signin = new JButton("Create an account");
+        signin.setHorizontalAlignment(SwingConstants.CENTER);
+        signin.setOpaque(true); signin.setBackground(Color.darkGray); signin.setForeground(Color.white);
+        signin.setFocusPainted(false); signin.setRolloverEnabled(false);
+        signin.setBorder(new EmptyBorder(10, 0, 10, 0));
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridy = 7; welcomePanel.add(signin, gbc);
 
-        welcomePanel.add(label);
-
-//        welcomePanel.add(inputPanel);
-        welcomePanel.add(emaillabel);
-        welcomePanel.add(email);
-
-        welcomePanel.add(psswlabel);
-        welcomePanel.add(password);
-
-        welcomePanel.add(login);
-
-        welcomePanel.add(or);
-        welcomePanel.add(signin);
         add(welcomePanel);
     }
 }
